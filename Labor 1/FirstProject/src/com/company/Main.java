5d76149ca1a1bae7422fa9f5623f4804f8e1ee32
peti2255 <48029773+peti2255@users.noreply.github.com>
2021-02-18 11:37:23 +0200

    Lab1 Feladatok elkezdése

--- Labor 1/FirstProject/src/com/company/Main.java
+++ Labor 1/FirstProject/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Erszény Péter\n\n");
-        System.out.println("sout\n");
+        System.out.println("\n");
 
         String nev = "Pentek Janos";
         for( int i=0; i<nev.length(); ++i){
