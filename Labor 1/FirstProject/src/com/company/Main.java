73a4ff07f4eb42c30b0a80da20ad0a6c37aae375
peti2255 <48029773+peti2255@users.noreply.github.com>
2021-02-22 21:48:27 +0200

    Lab1 Házi Feladat I. Egyszerű main függvénnyel megoldandó feladatok

--- Labor 1/FirstProject/src/com/company/Main.java
+++ Labor 1/FirstProject/src/com/company/Main.java
@@ -5,11 +5,22 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Erszény Péter\n\n");
         System.out.println("sout\n");
-
+        //2.Feladat
         String nev = "Pentek Janos";
         for( int i=0; i<nev.length(); ++i){
             System.out.println(nev.charAt(i));
         }
+        //3.Feladat
+        System.out.println("\n");
+        String nev2 = "ALMAFA";
+       for (int i=0;i<nev2.length();i++)
+       {
+           for (int j=0;j<i+1;j++)
+           {
+               System.out.print(nev2.charAt(j));
+           }
+           System.out.println();
 
+       }
     }
 }
