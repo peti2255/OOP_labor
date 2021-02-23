177ddb204c86076906420454ae58d9e501af433a
peti2255 <48029773+peti2255@users.noreply.github.com>
2021-02-23 20:33:21 +0200

    Hazi feladat 4

---
+++ Labor 1/Hazi feladat 4/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double[] array = {1.12, 2.01, 3.1, 4.123, 5.8654, 6, 7, 8, 9};
+        System.out.printf("%.2f",mean(array));
+    }
+
+    public static double mean(double array[]) {
+        double osszeg = 0;
+        for (int i = 0; i < array.length; i++) {
+            osszeg += array[i];
+        }
+        return osszeg / array.length;
+    }
+}
